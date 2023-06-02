dc3a141c1a335432a77e165e0dece568f0987d9b
Daexaf <ihsan64.mi@gmail.com>
2023-06-02 21:33:28 +0700

    Server untuk skripsi

---
+++ db/migrations/20230504155349-create-customers.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Customers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      no_telp: {
+        type: Sequelize.STRING
+      },
+      history_orders: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Customers');
+  }
+};
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+var corsOptions = {
+  origin: "http://localhost:8083/",
+};
+// app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to E-Duren.com" });
+});
+
+app.get("/products", (req, res) => {
+  res.json({ name: "Dax" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8083;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
